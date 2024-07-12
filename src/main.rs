fe5dda35f719d3ac6ce5e1b860d9ceb0e6132a69
Michael Amadi <amadimichaeld@gmail.com>
2024-07-12 13:09:49 +0100

    fix bugs, refactor and optimize ...

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,44 @@
 use flate2::{read::ZlibDecoder, write::ZlibEncoder, Compression};
 use sha1::{Digest, Sha1};
 use std::{
-    env, fs,
+    env,
+    ffi::OsString,
+    fs,
     io::{Read, Write},
+    path::{Path, PathBuf},
 };
 
 fn main() {
+    // Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
-    let command = args
-        .get(1)
-        .expect("expected a command, one of cat-file, hash-object, ls-tree, write-tree")
-        .as_str();
-
-    match command {
+    match args[1].as_str() {
         "init" => init(),
         "cat-file" => {
             let action = args.get(2).expect("expected -p, -t or -s");
             let hash = args.get(3).expect("expected hash");
-            println!("{}", cat_file(action, hash));
+            cat_file(action, hash);
         }
         "hash-object" => {
             let arg1 = args.get(2).expect("expected -w or file to hash");
 
             if arg1 == "-w" {
                 let hash = args.get(3).expect("expected hash");
-                println!("{}", hash_object(hash, true));
+                println!("{}", hex::encode(hash_object(hash, true)));
             } else {
-                println!("{}", hash_object(arg1, false));
+                println!("{}", hex::encode(hash_object(arg1, false)));
             }
         }
         "ls-tree" => {
-            let hash = args.get(2).expect("expected hash");
-            ls_tree(hash);
+            let action = args.get(2).expect("expected -p, -t or -s");
+            let hash = args.get(3).expect("expected hash");
+
+            ls_tree(action, hash);
         }
         "write-tree" => {
-            println!("{}", write_tree("./"));
+            println!("{}", hex::encode(write_tree("./")));
         }
         _ => println!("unknown command: {}", args[1]),
     }
-}
-
-fn get_hash_file(hash: &str) -> String {
-    let mut path = get_hash_dir(hash);
-    path.push_str("/");
-    path.push_str(hash.get(2..).unwrap());
-
-    return path;
-}
-
-fn get_hash_dir(hash: &str) -> String {
-    let mut path = String::from(".git/objects/");
-    path.push_str(hash.get(0..2).unwrap());
-
-    return path;
 }
 
 fn init() {
@@ -64,73 +50,45 @@ fn init() {
     println!("Iniitialized git directory");
 }
 
-fn cat_decoded(compressed_blob: &[u8]) -> ZlibDecoder<&[u8]> {
-    let decoded = ZlibDecoder::new(compressed_blob);
+fn cat_file(flag: &str, hash: &str) {
+    let mut path = String::from(".git/objects/");
+    path.push_str(hash.get(0..2).unwrap());
+    path.push_str("/");
+    path.push_str(hash.get(2..).unwrap());
 
-    return decoded;
-}
-
-fn cat_file(flag: &str, hash: &str) -> String {
-    let path = get_hash_file(hash);
-
-    let binding = fs::read(path).unwrap();
-    let mut decoded = cat_decoded(binding.as_slice());
-    let decoded2 = cat_decoded(binding.as_slice());
-
-    let mut hash_type = Vec::new();
-    let mut size = Vec::new();
-    let mut find_size = false;
-    for b in decoded2.bytes() {
-        let val = b.unwrap();
-
-        if val == b' ' {
-            find_size = true;
-            continue;
-        } else if val == b'\x00' {
-            break;
-        }
-
-        if find_size {
-            size.push(val);
-        } else {
-            hash_type.push(val);
-        }
-    }
-    let hash_type = String::from_utf8(hash_type).unwrap();
-    let size = String::from_utf8(size).unwrap();
+    let gz_blob = fs::read(path).unwrap();
+    let mut gz_blob = ZlibDecoder::new(gz_blob.as_slice());
 
     let mut output = String::new();
-    if hash_type == "blob" {
-        decoded.read_to_string(&mut output).unwrap();
+    gz_blob.read_to_string(&mut output).unwrap();
 
-        let arr = output.split("\x00").collect::<Vec<&str>>();
-        output = arr[1].to_owned();
-    } else {
-        output = String::new();
-    }
+    let arr = output.split("\x00").collect::<Vec<&str>>();
+    let output = arr[1];
+
+    let prefix = arr[0].split(" ").collect::<Vec<&str>>();
+    let hash_type = prefix[0];
+    let size = prefix[1];
 
     match flag {
-        "-p" => {
-            ls_tree(hash);
-            output
-        }
-        "-s" => size.to_string(),
-        "-t" => hash_type.to_owned(),
-        _ => format!("invalid flag: {}", flag),
+        "-p" => print!("{}", output),
+        "-s" => println!("{}", size),
+        "-t" => println!("{}", hash_type),
+        _ => println!("invalid flag: {}", flag),
     }
 }
 
-fn hash_object(file: &str, write: bool) -> String {
+fn hash_object(file: &str, write: bool) -> Vec<u8> {
     let contents = fs::read_to_string(file).unwrap();
     let mut complete = format!("blob {}\0", contents.len());
     complete.push_str(&contents);
     let mut hasher = Sha1::new();
     hasher.update(&complete);
     let hash = hasher.finalize();
-    let hash = hex::ToHex::encode_hex::<String>(&hash.to_vec());
 
     if write {
-        let mut path = get_hash_dir(&hash);
+        let hash = hex::encode(hash);
+        let mut path = String::from(".git/objects/");
+        path.push_str(hash.get(0..2).unwrap());
         let _ = fs::create_dir(&path);
 
         path.push_str("/");
@@ -140,12 +98,12 @@ fn hash_object(file: &str, write: bool) -> String {
         e.write_all(complete.as_bytes()).unwrap();
         let compressed = e.finish().unwrap();
 
-        // println!("file: {}", path);
+        // println!("{}", path);
 
         fs::write(path, compressed).unwrap();
     }
 
-    return hash;
+    hash.to_vec()
 }
 
 /**
@@ -153,80 +111,101 @@ fn hash_object(file: &str, write: bool) -> String {
  <mode> <name>\0<20_byte_sha>
  <mode> <name>\0<20_byte_sha>
 */
-fn ls_tree(hash: &str) {
-    let path = get_hash_file(hash);
-    let compressed_blob = fs::read(path).unwrap();
-    let decoded = cat_decoded(compressed_blob.as_slice());
+fn ls_tree(flag: &str, hash: &str) {
+    let mut path = String::from(".git/objects/");
+    path.push_str(hash.get(0..2).unwrap());
+    path.push_str("/");
+    path.push_str(hash.get(2..).unwrap());
 
-    let mut to_write = 0;
+    let gz_blob = fs::read(path).unwrap();
+    let gz_blob = ZlibDecoder::new(gz_blob.as_slice());
 
-    let mut header = Vec::new();
-    let mut mode = Vec::new();
-    let mut name = Vec::new();
-    let mut sha_hash = Vec::new();
+    let mut tree_type = vec![];
+    let mut tree_size = vec![];
 
-    let mut this_count = 0;
+    let mut modes = vec![];
+    let mut names = vec![];
+    let mut hashes = vec![];
 
-    for byte in decoded.bytes() {
+    let mut to_fill = 0;
+    let mut fill_tree_type = true;
+    let mut hash_count = 0;
+
+    for byte in gz_blob.bytes() {
         let val = byte.unwrap();
 
-        if to_write == 0 {
-            if val == b' ' {
-                if String::from_utf8(header.clone()).unwrap() != "tree"
-                    && String::from_utf8(header.clone()).unwrap() != "commit"
-                {
-                    println!("Not a tree hash type");
-                    break;
+        if to_fill == 0 {
+            match val {
+                b' ' => {
+                    fill_tree_type = false;
                 }
-            }
-
-            header.push(val);
-
-            if val == b'\x00' {
-                to_write = 1;
+                b'\x00' => {
+                    to_fill = 1;
+                    modes.push(vec![]);
+                    names.push(vec![]);
+                    hashes.push(vec![]);
+                }
+                _ => {
+                    if fill_tree_type {
+                        tree_type.push(val);
+                    } else {
+                        tree_size.push(val);
+                    }
+                }
             }
 
             continue;
-        }
-
-        if to_write == 1 {
-            mode.push(val);
-        } else if to_write == 2 {
-            name.push(val);
         } else {
-            this_count += 1;
-            sha_hash.push(val);
-        }
-
-        if val == b' ' && to_write != 3 {
-            mode.pop();
-            to_write = 2;
-        } else if val == b'\x00' && to_write != 3 {
-            name.pop();
-            to_write = 3;
-        } else if this_count == 20 {
-            this_count = 0;
-            to_write = 1;
-
-            let this_hash = hex::encode(sha_hash.as_slice());
-            let str_name = String::from_utf8(name.clone()).unwrap();
-            let mut str_mode = String::from_utf8(mode.clone()).unwrap();
-
-            let file_type = {
-                if str_mode.replace(" ", "") == "40000" {
-                    str_mode = "040000".to_owned();
-                    "tree"
-                } else {
-                    "blob"
+            match val {
+                b' ' => {
+                    to_fill = 2;
                 }
-            };
+                b'\x00' => {
+                    to_fill = 3;
+                }
+                _ => match to_fill {
+                    1 => {
+                        let len = modes.len();
+                        let latest: &mut Vec<u8> = &mut modes[len - 1];
+                        latest.push(val);
+                    }
+                    2 => {
+                        let len = names.len();
+                        let latest: &mut Vec<u8> = &mut names[len - 1];
+                        latest.push(val);
+                    }
+                    3 => {
+                        let len = hashes.len();
+                        let latest: &mut Vec<u8> = &mut hashes[len - 1];
+                        latest.push(val);
+                        hash_count += 1;
 
-            println!("{} {} {}    {}", str_mode, file_type, this_hash, str_name);
-
-            mode.clear();
-            name.clear();
-            sha_hash.clear();
+                        if hash_count == 20 {
+                            modes.push(vec![]);
+                            names.push(vec![]);
+                            hashes.push(vec![]);
+                            to_fill = 1;
+                            hash_count = 0;
+                        }
+                    }
+                    _ => unreachable!(),
+                },
+            }
         }
+    }
+
+    match flag {
+        "--name-only" => {
+            let last = names.len() - 1;
+            for (i, name) in names.into_iter().enumerate() {
+                if i == last {
+                    print!("{}", String::from_utf8(name).unwrap());
+                } else {
+                    print!("{}\n", String::from_utf8(name).unwrap());
+                }
+            }
+        }
+        _ => println!("Invalid flag"),
     }
 }
 
@@ -235,79 +214,83 @@ fn ls_tree(hash: &str) {
  <mode> <name>\0<20_byte_sha>
  <mode> <name>\0<20_byte_sha>
 */
-fn write_tree(path: &str) -> String {
-    let mut content = Vec::<u8>::new();
-    let mut working_area = fs::read_dir(&path)
+fn write_tree(path: &str) -> Vec<u8> {
+    let mut working_directory = fs::read_dir(path)
         .unwrap()
-        .map(|e| e.unwrap().file_name().into_string().unwrap())
-        .collect::<Vec<String>>();
-    working_area.sort();
+        .map(|e| {
+            let b = e.unwrap().path();
+            b.as_os_str().to_owned()
+        })
+        .collect::<Vec<OsString>>();
+    working_directory.sort();
 
-    let ignored = fs::read_to_string(".gitignore");
+    let working_directory = working_directory
+        .iter()
+        .map(|e| Path::new(e.as_os_str()))
+        .collect::<Vec<&Path>>();
 
-    let mut ignored = match ignored {
-        Ok(ref files) => files
-            .split("\n")
-            .map(|e| {
-                let mut r = e;
-                if e.starts_with('/') {
-                    r = e.split_at(1).1;
-                } else if e.ends_with("/") {
-                    r = e.split_at(e.len() - 1).0;
-                }
-                r
-            })
-            .collect(),
-        Err(_) => Vec::new(),
-    };
-    ignored.push(".git");
+    let ignored = fs::read_to_string(".gitignore").unwrap_or_default();
+    let mut ignored = ignored
+        .split('\n')
+        .filter_map(|e| match e.split_once('#').unwrap_or((e, "")).0 {
+            "" => None,
+            _ => Path::new(e).canonicalize().ok(),
+        })
+        .collect::<Vec<PathBuf>>();
+    ignored.push(Path::new(".git").canonicalize().unwrap());
 
-    for file in working_area {
-        if ignored.contains(&file.as_str()) {
+    let mut contents = vec![];
+
+    // println!("{:?}", working_directory);
+    // println!("ignored: {:?}", ignored);
+
+    for file in &working_directory {
+        if ignored.contains(&file.canonicalize().unwrap()) {
             continue;
         }
 
-        let is_dir = fs::metadata(&file).unwrap().file_type().is_dir();
+        if file.is_file() {
+            contents.extend("100644 ".as_bytes().to_vec());
+        } else {
+            contents.extend("40000 ".as_bytes().to_vec());
+        }
 
-        let (mode, hash) = {
-            if is_dir {
-                let mut binding = file.clone();
-                binding.push('/');
-                ("40000".to_owned(), write_tree(&binding))
-            } else {
-                let mut this_path = path.to_owned();
-                this_path.push_str(&file);
+        contents.extend(file.file_name().unwrap().as_encoded_bytes().to_vec());
+        contents.push(b'\0');
 
-                ("100644".to_owned(), hash_object(&this_path, true))
-            }
-        };
-
-        content.append(&mut format!("{} {}\x00", mode, file).as_bytes().to_vec());
-        content.append(&mut hex::decode(hash).unwrap());
+        if file.is_file() {
+            let hash = hash_object(
+                &String::from_utf8(file.as_os_str().as_encoded_bytes().to_vec()).unwrap(),
+                true,
+            );
+            contents.extend(hash);
+        } else {
+            let hash = write_tree(
+                &String::from_utf8(file.as_os_str().as_encoded_bytes().to_vec()).unwrap(),
+            );
+            contents.extend(hash);
+        }
     }
 
-    // Hash it
     let mut hasher = Sha1::new();
-    hasher.update(&content);
+    hasher.update(&contents);
     let hash = hasher.finalize();
 
-    // append prefix of file type and size
-    let mut to_encode = format!("tree {}", content.len()).as_bytes().to_vec();
-    to_encode.push(b'\x00');
-    to_encode.append(&mut content);
+    let mut complete = format!("tree {}\0", contents.len()).as_bytes().to_vec();
+    complete.extend(contents);
 
-    // encode and write to file
     let hex_hash = hex::encode(&hash);
-    let mut path = get_hash_dir(&hex_hash);
-    let _ = fs::create_dir(&path);
-    path.push_str("/");
-    path.push_str(hex_hash.get(2..).unwrap());
+    let mut this_path = String::from(".git/objects/");
+    this_path.push_str(&hex_hash[0..2]);
+    let _ = fs::create_dir(&this_path);
+    this_path.push('/');
+    this_path.push_str(&hex_hash[2..]);
 
-    let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
-    e.write_all(&to_encode).unwrap();
-    let compressed = e.finish().unwrap();
+    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::default());
+    encoder.write_all(&complete).unwrap();
+    let compressed = encoder.finish().unwrap();
 
-    fs::write(path, compressed).unwrap();
+    fs::write(this_path, compressed).unwrap();
 
-    return hex_hash;
+    hash.to_vec()
 }
